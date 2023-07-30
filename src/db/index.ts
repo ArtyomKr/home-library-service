26646f7c290e2f5fb82fcfd02d43360a30dcb7f7
ArtyomKr <tyomik.krasnitsky@gmail.com>
2023-07-30 18:31:15 +0300

    feat: add favourites to db

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,17 +1,27 @@
 import { User } from '../user/entities/user.entity';
 import { Track } from '../track/entities/track.entity';
 import { Artist } from '../artist/entities/artist.entity';
+import { Album } from '../album/entities/album.entity';
+import { Favourites } from '../favourites/entities/favourites.entity';
 
 interface IDB {
   users: User[];
   tracks: Track[];
   artists: Artist[];
+  albums: Album[];
+  favourites: Favourites;
 }
 
 const db: IDB = {
   users: [],
   tracks: [],
   artists: [],
+  albums: [],
+  favourites: {
+    artists: [],
+    albums: [],
+    tracks: [],
+  },
 };
 
 export const getDB = () => db;

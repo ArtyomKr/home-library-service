b7ddd00bf83f99917b5a72752e1bf56c74f88d21
ArtyomKr <tyomik.krasnitsky@gmail.com>
2023-08-04 18:12:43 +0300

    feat: add postgres connection and user table

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './routes/user/user.module';
@@ -7,6 +8,10 @@ import { ArtistModule } from './routes/artist/artist.module';
 import { AlbumModule } from './routes/album/album.module';
 import { FavouritesModule } from './routes/favourites/favourites.module';
 
+const username = process.env.POSTGRES_USER || 'postgres';
+const password = process.env.POSTGRES_PASSWORD || '12345678';
+const database = process.env.POSTGRES_DB || 'postgres';
+
 @Module({
   imports: [
     UserModule,
@@ -14,6 +19,17 @@ import { FavouritesModule } from './routes/favourites/favourites.module';
     ArtistModule,
     AlbumModule,
     FavouritesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username,
+      password,
+      database,
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/routes/user/entities/user.entity.ts
+++ src/routes/user/entities/user.entity.ts
@@ -1,8 +1,38 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
 export class User {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column({ unique: true })
   login: string;
+
+  @Column()
   password: string;
+
+  @Column()
   version: number;
+
+  @CreateDateColumn({
+    transformer: {
+      to: (value: Date) => value,
+      from: (value: Date) => value.getTime(),
+    },
+  })
   createdAt: number;
+
+  @UpdateDateColumn({
+    transformer: {
+      to: (value: Date) => value,
+      from: (value: Date) => value.getTime(),
+    },
+  })
   updatedAt: number;
 }

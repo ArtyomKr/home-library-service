834dfaa74acf990c21ae35614c6a8af0b137f25c
ArtyomKr <tyomik.krasnitsky@gmail.com>
2023-08-08 15:45:41 +0300

    fix: make both db ports variables

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -1,16 +1,17 @@
 import { ConnectionOptions, DataSource } from 'typeorm';
 import * as dotenv from 'dotenv';
-dotenv.config();
 
+dotenv.config();
 const username = process.env.POSTGRES_USER ?? 'postgres';
 const password = process.env.POSTGRES_PASSWORD ?? '12345678';
 const database = process.env.POSTGRES_DB ?? 'postgres';
 const host = process.env.DB_HOST ?? 'localhost';
+const port = +(process.env.DB_PORT ?? 5432);
 
 export const connectionOptions: ConnectionOptions = {
   type: 'postgres',
   host,
-  port: 5432,
+  port,
   username,
   password,
   database,

42f6f70c9ff1f098673773fe8078978c22c21d46
ArtyomKr <tyomik.krasnitsky@gmail.com>
2023-08-18 16:57:34 +0300

    feat: update auth service to use login functionality

--- src/routes/auth/auth.service.ts
+++ src/routes/auth/auth.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
-import { RefreshDto } from './dto/refresh.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../user/entities/user.entity';
 import { Repository } from 'typeorm';
 import { BusinessError } from '../../utils/businessError';
 import { compare } from 'bcrypt';
-import generateToken from '../../utils/generateToken';
+import { generateAccessToken } from '../../utils/generateToken';
+import { TokenDto } from './dto/token.dto';
 
 @Injectable()
 export class AuthService {
@@ -15,11 +15,13 @@ export class AuthService {
     private usersRepository: Repository<User>,
   ) {}
 
-  async login({ login, password }: LoginDto) {
+  async login({ login, password }: LoginDto): Promise<TokenDto> {
     const user = await this.usersRepository.findOne({ where: { login } });
-    if (!user) throw new BusinessError('User not found', 404);
-    if (!(await compare(password, user.password)))
-      throw new BusinessError('Password is incorrect', 403);
-    return generateToken(user.id, login);
+    if (!user || !(await compare(password, user.password)))
+      throw new BusinessError(
+        'Password is incorrect or user does not exist',
+        403,
+      );
+    return { token: generateAccessToken(user.id, login) };
   }
 }

d8dc72b2b9d0eb3dde87d168d322dcc97ded8d17
ArtyomKr <tyomik.krasnitsky@gmail.com>
2023-08-18 16:49:26 +0300

    feat: add functions for access and refresh tokens

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -1,7 +1,20 @@
 import { sign } from 'jsonwebtoken';
 
-function generateToken(id: string, login: string) {
-  return sign({ userId: id, login }, process.env.JWT_SECRET_KEY as string);
+const accessTokenExpiration = process.env.ACCESS_TOKEN_EXPIRATION ?? '12h';
+const refreshTokenExpiration = process.env.REFRESH_TOKEN_EXPIRATION ?? '48h';
+
+export function generateAccessToken(id, login) {
+  return sign({ userId: id, login }, process.env.JWT_SECRET_KEY as string, {
+    expiresIn: accessTokenExpiration,
+  });
 }
 
-export default generateToken;
+export function generateRefreshToken(id, login) {
+  return sign(
+    { userId: id, login },
+    process.env.JWT_SECRET_REFRESH_KEY as string,
+    {
+      expiresIn: refreshTokenExpiration,
+    },
+  );
+}
